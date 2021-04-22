386c602fba540abe1915f727015a4427081da78f
Gustavo Galupo <ggalupo2@hotmail.com>
2021-04-22 20:55:14 -0300

    Import fake API data from json-server in Home page using SSG.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
-export default function Home() {
+import { GetStaticProps } from 'next'
+
+type Episode = {
+  id: string
+  title: string
+  members: string //....
+}
+
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 6,
+  }
 }
