6203a5a7a41a0008d99d628e2058d00fc00e56f0
Gustavo Galupo <ggalupo2@hotmail.com>
2021-04-23 16:24:58 -0300

    Routing to episodes pages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { GetStaticProps } from 'next'
 import { format, parseISO } from 'date-fns'
 import enUS from 'date-fns/locale/en-US'
 import { api } from '../services/api'
 import { convertDurationToTimeString } from '../utils/durationToTimeString'
 
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from './home.module.scss'
 
@@ -43,7 +44,9 @@ export default function Home({ latestEpisodes, olderEpisodes }: HomeProps) {
                   />
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -61,12 +64,14 @@ export default function Home({ latestEpisodes, olderEpisodes }: HomeProps) {
           <h2>Older Podcasts</h2>
           <table cellSpacing={0}>
             <thead>
-              <th></th>
-              <th>Podcast</th>
-              <th>Members</th>
-              <th>Date</th>
-              <th>Duration</th>
-              <th></th>
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Members</th>
+                <th>Date</th>
+                <th>Duration</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
               {olderEpisodes.map(episode => {
@@ -81,7 +86,9 @@ export default function Home({ latestEpisodes, olderEpisodes }: HomeProps) {
                         objectFit="cover"/>
                     </td>
                     <td>
-                      <a href="">{episode.title}</a>
+                      <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                      </Link>
                     </td>
                     <td>{episode.members}</td>
                     <td>{episode.publishedAt}</td>
